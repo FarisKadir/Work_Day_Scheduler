7303923e7fb72c9546bf22056413c353ecc3600a
FarisKadir <faris.kadir@gmail.com>
2021-01-21 19:59:42 -0500

    Updated the index.html file to use semantics. Update js file to try different code for the loop.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*Psuedocode
 When the page loads
-    Display the current date at the top
+    Display the current date at the top - Done
     Display timeslots for each hour of the day
         Each timeslot should contain:
             A time, ex: 9 am
@@ -16,7 +16,44 @@ When the page loads
                 When the save button is clicked it should save the entry from the input box to local storage.
 */
 
+//start of document ready
+$(document).ready(function()    {
+
+    //Set today's date
+    $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+    var now = moment();
 
 
-//Set today's date
-$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+    //Variables to keep adding rows
+    function createTime()    {
+        var times = [9,10,11,12,1,2,3,4,5];
+        var now = moment();
+        
+
+
+        for (var i = 0; i < times.length; i++)  {
+            var div = $("div");
+            div.addClass("row time-block");
+            div.attr("data-time", times[i])
+            $(".container").append(div);
+            // $(".container").append("<div class ='row timeblock' id=" + times[i] + "></div>");
+            // if (times[i] < now) {
+            //     div.addClass("past");
+            // }
+            // console.log(i);
+        };
+
+
+
+    }
+
+    createTime();
+    
+
+
+
+
+
+
+}) //End of document ready
+
