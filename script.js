06daf555e5ef635014beeebebe34278e0fe3ae00
FarisKadir <faris.kadir@gmail.com>
2021-01-21 21:07:26 -0500

    reworking the code to create the table rows in js.

--- script.js
+++ script.js
@@ -18,29 +18,54 @@ When the page loads
 
 //start of document ready
 $(document).ready(function()    {
+    //An array for the time slots
+    var times = [9,10,11,12,13,14,15,16,17,18,19,20];
 
     //Set today's date
     $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
-    var now = moment();
+
+    //retrieve the current hour
+    var now = parseInt(moment().format('h'));
 
 
     //Variables to keep adding rows
     function createTime()    {
-        var times = [9,10,11,12,1,2,3,4,5];
-        var now = moment();
-        
-
-
         for (var i = 0; i < times.length; i++)  {
-            var div = $("div");
-            div.addClass("row time-block");
-            div.attr("data-time", times[i])
-            $(".container").append(div);
-            // $(".container").append("<div class ='row timeblock' id=" + times[i] + "></div>");
+            var tRow = $("<tr>");
+            var tData = $("<td>");
+
+            if (times[i] <= now) {
+                tRow.addClass("row time-block past");
+                tRow.attr("colspan","800");
+                $(".timeslots").append(tRow);
+                $(".time-block").append(tData);
+                tData.addClass("hour");
+
+            }
+
+
+
+
+
+
+
+            tRow.addClass("row time-block");
+
+
+            // div.attr("data-time", times[i])
+            
+            // $(".timeslots").append(div);
+            
+            
             // if (times[i] < now) {
             //     div.addClass("past");
+            //     $(".time-block").append(span);
             // }
-            // console.log(i);
+            // else    {
+            //     div.addClass("future");
+            //     $(".time-block").append(span);
+            // }
+
         };
 
 
