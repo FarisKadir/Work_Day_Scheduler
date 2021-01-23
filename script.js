608e814e163be87d48624471898156d2f884af56
FarisKadir <faris.kadir@gmail.com>
2021-01-22 21:40:19 -0500

    Removed excessive classes from the index.html elements. Added IDs and names to all of the text areas. Started script.js file over from scratch.

--- script.js
+++ script.js
@@ -21,80 +21,13 @@ $(document).ready(function()    {
 
     //Get today's date
     var today = moment().format("dddd, MMMM Do, YYYY");
-    var now = moment().format('LT');
-    console.log("This is the current time: " + now);
 
-    //An array for the time slots
-    var times = [9,10,11,12,13,14,15,16,17]
-
-   
-    var slot = moment().set({"hour": 9, "minute": 00, "seconds": 00, "millisecond": 00}).format('LT');
-
-    console.log("This is the timeslot: " + slot);
-
-    console.log(now +" " + slot);
-
-
-
-    // if (moment(slot).isBefore(now))    {
-    //     console.log("Current Time: " + now);
-    //     console.log("Timeslot: " + slot);
-    //     console.log("This is in the past");
-    // }
-    // else if (now === slot) {
-    //     console.log("Today's date/time: " + now);
-    //     console.log("Timeslot: " + slot);
-    //     console.log("This is the present")
-    // }
-    // else if (slot > now)    {
-    //     console.log("Today's date/time: " + now);
-    //     console.log("Timeslot: " + slot);
-    //     console.log("This is in the future")
-    // }
-
-
-    //Set today's date
+    //Set today's date in the header
     $("#currentDay").text(today);
 
-    //retrieve the current hour
 
 
+    
 
-    //Variables to keep adding rows
-    function createTime()    {
-        var tRow = $("<tr>");
-        var tData = $("<td>");
-        function createRow(x)   {
-            tRow.addClass("row time-block " + x);
-            tRow.attr("colspan","800");
-            $(".timeslots").append(tRow);
-            $(".time-block").append(tData);
-            tData.addClass("hour");
-        }
-
-        for (var i = 0; i < times.length; i++)  {
-            if (times[i] < now) {
-               createRow("past");
-               console.log("This is in the past");
-               break;
-            }
-            else if (times[i] === now)  {
-                createRow("present");
-                console.log("This is in the present");
-                break;
-            }
-            else    {
-                createRow("future");
-                console.log("This is in the future");
-                break;
-            }
-
-            tRow.addClass("row time-block");
-
-        };
-
-
-
-    }
-//End of document ready
 });
+//End of document ready
