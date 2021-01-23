9055c864f377fabc34b6cd222a773abb87d8eaf6
FarisKadir <faris.kadir@gmail.com>
2021-01-23 17:06:18 -0500

    Added data-time attribute to all buttons. Added data-mil attribute to all time-block divs. Fixed code that changes the time-block's color. Finished code that stores the tasks when the save button is clicked. Finished code that displays existing tasks when the page loads.

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
-/*Psuedocode
-When the page loads
-    Display the current date at the top - Done
-    Display timeslots for each hour of the day
-        Each timeslot should contain:
-            A time, ex: 9 am
-                Assign a color to the time slot boxes:
-                    The current time's timeslot should have a red background.
-                    Any timeslots that are in the past should have a gray background.
-                        Would need to capture the current time.
-                    Any timeslots that are in the future should have a green background.
-                The boxes only have borders on the top, bottom, and left.
-                
-            An input box to type in events.
-            A save button.
-                When the save button is clicked it should save the entry from the input box to local storage.
-*/
-
 //start of document ready
 $(document).ready(function()    {
+    //Defining Global Variables
 
     //Get today's date
     var today = moment().format("dddd, MMMM Do, YYYY");
+    var today2 = moment().format("MM/DD/YYYY");
+
+    //get the current hour in the 24 hour format
+    var now = parseInt(moment().format("k"));
+
+    
+    ///Actions that take place when the page loads:
 
     //Set today's date in the header
     $("#currentDay").text(today);
 
 
+    //Changes the color of the timeblock to past, present, future
+    $("div").each(function()    {
+        //Store the current data-mil value
+        var dataMil = $(this).attr("data-mil");
 
-    
+        //
+        if (typeof parseInt(dataMil) === "number")    {
+            
+            if (now > dataMil)  {
+                $(this).addClass("past");
+            }
+            else if (now == dataMil)   {
+                $(this).addClass("present");
+            }
+            else if (now < dataMil) {
+                $(this).addClass("future");
+            };
+        }
+    })
+
+
+    //On Click Event
+
+    $(".saveBtn").on("click",function() {
+        var dataTime = $(this).attr("data-time")
+        var userInput = $("#" + dataTime + "_tasks").val();
+        var task = {
+            date: today2,
+            time: dataTime,
+            task: userInput,
+        };
+        localStorage.setItem(today2 + " at " + dataTime +"_tasks", JSON.stringify(task));
+    })
+
+    //Pre-Load existing tasks
+
+    $("textarea").each(function()   {
+        var id = $(this).attr("id");
+        var key = today2 + " at " + id;
+        var obj = JSON.parse(localStorage.getItem(key));
+        if (!!obj && obj.date === today2)    {
+            $(this).val(obj.task);
+        };
+        
+
+    })
 
 });
 //End of document ready
